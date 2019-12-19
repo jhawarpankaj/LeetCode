f50ecc17320a2e2876bfd0761c384708d04fbce3
Pankaj Kr Jhawar <jhawar.pankaj1990@gmail.com>
2019-12-19 01:37:24 -0600

    Create tree-from-inorder-preorder.java

---
+++ Tree/tree-from-inorder-preorder.java
@@ -0,0 +1,23 @@
+
+class Solution {
+    Map<Integer, Integer> hm = new HashMap<Integer, Integer>();
+    int idx = 0;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for(int i = 0; i < inorder.length; i++){
+            hm.put(inorder[i], i);
+        }
+        return helper(0, inorder.length - 1, inorder, preorder);
+    }
+    
+    TreeNode helper(int l, int h, int[] inorder, int[] preorder){
+        if(l > h) return null;
+        int rootVal = preorder[idx++];
+        if(l == h) return new TreeNode(rootVal);
+        TreeNode root = new TreeNode(rootVal);
+        // idx++;
+        int index = hm.get(rootVal);
+        root.left = helper(l, index - 1, inorder, preorder);
+        root.right = helper(index + 1, h, inorder, preorder);
+        return root;
+    }
+}
