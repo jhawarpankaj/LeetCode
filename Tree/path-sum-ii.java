72d35601721bb04c160760acc31e3a8735dd3a19
Pankaj Kr Jhawar <jhawar.pankaj1990@gmail.com>
2019-12-29 17:50:40 -0600

    Update path-sum-ii.java

--- Tree/path-sum-ii.java
+++ Tree/path-sum-ii.java
@@ -1,25 +1,25 @@
 
-Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+// Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
 
-Note: A leaf is a node with no children.
+// Note: A leaf is a node with no children.
 
-Example:
+// Example:
 
-Given the below binary tree and sum = 22,
+// Given the below binary tree and sum = 22,
 
-      5
-     / \
-    4   8
-   /   / \
-  11  13  4
- /  \    / \
-7    2  5   1
-Return:
+//       5
+//      / \
+//     4   8
+//    /   / \
+//   11  13  4
+//  /  \    / \
+// 7    2  5   1
+// Return:
 
-[
-   [5,4,11,2],
-   [5,8,4,5]
-]
+// [
+//    [5,4,11,2],
+//    [5,8,4,5]
+// ]
 
 
 class Solution {
