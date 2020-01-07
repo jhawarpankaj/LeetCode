355afa07be112772f7193c3715fed8785e4e3244
Pankaj Kr Jhawar <jhawar.pankaj1990@gmail.com>
2020-01-06 23:29:04 -0600

    Update all-nodes-distance-K.java

--- Tree/all-nodes-distance-K.java
+++ Tree/all-nodes-distance-K.java
@@ -2,7 +2,7 @@
 
 We are given a binary tree (with root node root), a target node, and an integer value K.
 
-Return a list of the values of all nodes that have a distance K from the target node.  The answer can be returned in any order.
+Return a list of the values of all nodes that have a distance K from the target node. The answer can be returned in any order.
 
 Example 1:
 
