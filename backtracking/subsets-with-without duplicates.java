2f0b3f6b045df89cfdad26636fb2ebd259300342
Pankaj Kr Jhawar <jhawar.pankaj1990@gmail.com>
2020-02-15 01:45:12 -0600

    Update and rename generate-all-subset.java to subsets-with-without duplicates.java

--- backtracking/subsets-with-without duplicates.java
+++ backtracking/subsets-with-without duplicates.java
@@ -73,3 +73,44 @@ class Solution {
         return result;
     }    
 }
+
+/*
+Subsets II
+
+Given a collection of integers that might contain duplicates, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: [1,2,2]
+Output:
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
+*/
+
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        backtrack(result, nums, 0, new ArrayList<Integer>());
+        return result;
+    }
+    
+    // start to avoid duplicate result like [1, 2] and [2, 1], i.e., not going back.
+    void backtrack(List<List<Integer>> result, int[] nums, int start, List<Integer> temp) {
+        result.add(new ArrayList<Integer>(temp));
+        for(int i = start; i < nums.length; i++) {
+            if(i > start && nums[i] == nums[i - 1]) continue; // to avoid duplicate result like [1, 2] and [1, 2]
+            temp.add(nums[i]);
+            backtrack(result, nums, i + 1, temp);
+            temp.remove(temp.size() - 1);
+        }
+    }    
+}
