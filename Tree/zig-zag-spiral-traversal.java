80f78d1bf5013d00ad0b2c2b7eda1654fe24897a
Pankaj Kr Jhawar <jhawar.pankaj1990@gmail.com>
2020-03-18 19:17:33 -0500

    Update zig-zag-spiral-traversal.java

--- Tree/zig-zag-spiral-traversal.java
+++ Tree/zig-zag-spiral-traversal.java
@@ -1,3 +1,36 @@
+// Using two stacks...
+
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if(root == null) return result;
+        Stack<TreeNode> stack1 = new Stack<TreeNode>();
+        Stack<TreeNode> stack2 = new Stack<TreeNode>();
+        stack1.push(root);
+        while(!stack1.isEmpty() || !stack2.isEmpty()) {
+            List<Integer> temp = new ArrayList<Integer>();
+            while(!stack1.isEmpty()) {
+                TreeNode curr = stack1.pop();
+                temp.add(curr.val);
+                if(curr.left != null) stack2.push(curr.left);
+                if(curr.right != null) stack2.push(curr.right);
+            }
+            result.add(new ArrayList<Integer>(temp));
+            temp.clear();
+            while(!stack2.isEmpty()) {
+                TreeNode curr = stack2.pop();
+                temp.add(curr.val);
+                if(curr.right != null) stack1.push(curr.right);
+                if(curr.left != null) stack1.push(curr.left);
+            }
+            if(!temp.isEmpty()) result.add(new ArrayList<Integer>(temp));
+        }
+        return result;
+    }
+}
+
+// Using a Queue and odd and even level...
+
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
