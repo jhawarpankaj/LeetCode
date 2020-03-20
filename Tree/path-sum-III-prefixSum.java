e1b98c7e90b7b7a7ae9cee1bdd9f71751f5a3107
Pankaj Kr Jhawar <jhawar.pankaj1990@gmail.com>
2020-03-20 15:45:17 -0500

    Update path-sum-III-prefixSum.java

--- Tree/path-sum-III-prefixSum.java
+++ Tree/path-sum-III-prefixSum.java
@@ -8,6 +8,11 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+/* This problem is same as the prefix sum. Look at the path in the tree as a continuous array. It's same as the continous
+subarray problem.
+*/
+
 class Solution {
     public int pathSum(TreeNode root, int sum) {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
@@ -20,10 +25,9 @@ class Solution {
         currSum = currSum + root.val;
         int count = map.getOrDefault(currSum - target, 0);
         map.put(currSum, map.getOrDefault(currSum, 0) + 1);
-        int res = count + getSum(root.left, currSum, target, map) + getSum(root.right, currSum, target, map);
-        
+        int res = count + getSum(root.left, currSum, target, map) + getSum(root.right, currSum, target, map);        
         // once we are done exploring the subtree and going back bottom up,
-        // we want to forget the sum added to map because of the children node.
+        // we want to forget the sum added to map because of the current node.
         map.put(currSum, map.get(currSum) - 1);
         return res;
     }
