78264ff742c22a2bc18879c44e62e92d5f96590e
Pankaj Kr Jhawar <jhawar.pankaj1990@gmail.com>
2020-07-02 23:56:24 -0700

    Update bst-to-gst.java

--- binary-search-tree/bst-to-gst.java
+++ binary-search-tree/bst-to-gst.java
@@ -1,36 +1,53 @@
-// Recursive
+/*
+https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/
 
+Given the root of a binary search tree with distinct values, modify it so that every node has a new value equal to the sum of the 
+values of the original tree that are greater than or equal to node.val.
+
+As a reminder, a binary search tree is a tree that satisfies these constraints:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+Input: [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+ 
+Constraints:
+
+The number of nodes in the tree is between 1 and 100.
+Each node will have value between 0 and 100.
+The given tree is a binary search tree.
+*/
+
+// Recursive
 class Solution {
-    int sum = 0;
-    public TreeNode convertBST(TreeNode root) {
-        helper(root);
+    int temp = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        if (root == null) return null;
+        bstToGst(root.right);
+        root.val += temp;
+        temp = root.val;
+        bstToGst(root.left);
         return root;
-    }
-    
-    void helper(TreeNode root){
-        if(root == null) return;
-        helper(root.right);
-        sum = sum + root.val;
-        root.val = sum;
-        helper(root.left);
     }
 }
 
 // Iterative
-
 class Solution {
-    public TreeNode convertBST(TreeNode root) {
-        Stack<TreeNode> stack = new Stack<TreeNode>();
+    public TreeNode bstToGst(TreeNode root) {
+        if (root == null) return null;
+        Stack<TreeNode> stack = new Stack<>();
         TreeNode curr = root;
-        int sum = 0;
-        while(!stack.isEmpty() || curr != null){
-            while(curr != null){
+        int temp = 0;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
                 stack.push(curr);
                 curr = curr.right;
             }
             curr = stack.pop();
-            sum = sum + curr.val;
-            curr.val = sum;
+            curr.val += temp;
+            temp = curr.val;
             curr = curr.left;
         }
         return root;
