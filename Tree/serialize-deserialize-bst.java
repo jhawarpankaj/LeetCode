a146543f517ff06f75cab5e7de47169c493a7e28
Pankaj Kr Jhawar <jhawar.pankaj1990@gmail.com>
2020-07-03 20:43:23 -0700

    Update serialize-deserialize-bst.java

--- Tree/serialize-deserialize-bst.java
+++ Tree/serialize-deserialize-bst.java
@@ -5,43 +5,40 @@ public class Codec {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        StringBuilder sb = new StringBuilder("");
-        if(root == null) return sb.toString();
-        postorder(root, sb);
-        sb.deleteCharAt(sb.length() - 1);
+        StringBuilder sb = new StringBuilder();
+        preorder(root, sb);
         return sb.toString();
     }
     
-    void postorder(TreeNode root, StringBuilder sb) {
-        if(root == null) return;        
-        postorder(root.left, sb);
-        postorder(root.right, sb);
-        sb.append(root.val + ",");
+    // preorder to get comma separated string with node's values.
+    void preorder(TreeNode root, StringBuilder sb) {
+        if (root == null) return;
+        sb = sb.append(root.val + ",");
+        preorder(root.left, sb);
+        preorder(root.right, sb);
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        if(data.isEmpty()) return null;
-        String[] postorder = data.split(",");
-        for(String s : postorder) {
-            System.out.print(s + ", ");
+        if (data.isEmpty()) return null;
+        // refresh reading about split function in java, here: https://docs.google.com/document/d/1kddefrMmtwtIRGkUCH0UZ3zplT07Dnw-h1PPvYn4AuE/edit
+        // because of the split function we have to specifically check the first empty string.
+        String[] split = data.split(","); 
+        Queue<Integer> Q = new LinkedList<>();
+        for (String val : split) {
+            Q.add(Integer.parseInt(val));
         }
-        Deque<Integer> stack = new ArrayDeque<Integer>();
-        for(String val : postorder) {
-            stack.push(Integer.valueOf(val));
-        }
-        
-        return rdeserialize(stack, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        return helper(Q, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
     
-    public TreeNode rdeserialize(Deque<Integer> stack, int lower, int upper) {
-        if(stack.isEmpty()) return null;
-        int val = stack.peek();
-        if(val < lower || val > upper) return null;
-        stack.pop();
-        TreeNode root = new TreeNode(val);
-        root.right = rdeserialize(stack, val, upper);
-        root.left = rdeserialize(stack, lower, val);
+    TreeNode helper(Queue<Integer> Q, int lower, int upper) {
+        if (Q.isEmpty()) return null;
+        int curr = Q.peek();
+        if (curr < lower || curr > upper) return null;
+        Q.remove();
+        TreeNode root = new TreeNode(curr);
+        root.left = helper(Q, lower, curr);
+        root.right = helper(Q, curr, upper);
         return root;
     }
 }
