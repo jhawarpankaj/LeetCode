392facb52e363a44f2677812f1fdb951c6deb269
Pankaj Kr Jhawar <jhawar.pankaj1990@gmail.com>
2020-07-05 14:06:07 -0700

    Update zig-zag-spiral-traversal.java

--- Tree/zig-zag-spiral-traversal.java
+++ Tree/zig-zag-spiral-traversal.java
@@ -1,5 +1,25 @@
-// Using two stacks...
+/*
+https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
 
+Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its zigzag level order traversal as:
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+
+*/
+
+// Using two stacks, bad solution.
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
@@ -29,8 +49,7 @@ class Solution {
     }
 }
 
-// Using a Queue and odd and even level...
-
+// Using a Queue and Deque or LinkedList for odd/even level.
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
