692c6983e14f04c25ca9af3ba868dd9fe2219eef
Pankaj Kr Jhawar <jhawar.pankaj1990@gmail.com>
2020-07-07 15:15:34 -0700

    Create intersection-of-two-linked-lists.java

---
+++ LinkedLists/intersection-of-two-linked-lists.java
@@ -0,0 +1,187 @@
+/*
+https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+Write a program to find the node at which the intersection of two singly linked lists begins.
+
+For example, the following two linked lists:
+Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+Output: Reference of the node with value = 8
+Input Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect). From the head of A, it reads as [4,1,8,4,5]. 
+From the head of B, it reads as [5,6,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+*/
+
+// The code will work even if there is no intersection.
+// As, after second iteration, both of them will be equal to null at the same time and the while loop will terminate.
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        return a;
+    }
+}
+
+/*
+Explanation:
+
+Visualization of this solution:
+Case 1 (Have Intersection & Same Len):
+
+       a
+A:     a1 → a2 → a3
+                   ↘
+                     c1 → c2 → c3 → null
+                   ↗            
+B:     b1 → b2 → b3
+       b
+            a
+A:     a1 → a2 → a3
+                   ↘
+                     c1 → c2 → c3 → null
+                   ↗            
+B:     b1 → b2 → b3
+            b
+                 a
+A:     a1 → a2 → a3
+                   ↘
+                     c1 → c2 → c3 → null
+                   ↗            
+B:     b1 → b2 → b3
+                 b
+A:     a1 → a2 → a3
+                   ↘ a
+                     c1 → c2 → c3 → null
+                   ↗ b            
+B:     b1 → b2 → b3
+Since a == b is true, end loop while(a != b), return the intersection node a = c1.
+
+Case 2 (Have Intersection & Different Len):
+
+            a
+A:          a1 → a2
+                   ↘
+                     c1 → c2 → c3 → null
+                   ↗            
+B:     b1 → b2 → b3
+       b
+                 a
+A:          a1 → a2
+                   ↘
+                     c1 → c2 → c3 → null
+                   ↗            
+B:     b1 → b2 → b3
+            b
+A:          a1 → a2
+                   ↘ a
+                     c1 → c2 → c3 → null
+                   ↗            
+B:     b1 → b2 → b3
+                 b
+A:          a1 → a2
+                   ↘      a
+                     c1 → c2 → c3 → null
+                   ↗ b           
+B:     b1 → b2 → b3
+A:          a1 → a2
+                   ↘           a
+                     c1 → c2 → c3 → null
+                   ↗      b           
+B:     b1 → b2 → b3
+A:          a1 → a2
+                   ↘                a = null, then a = b1
+                     c1 → c2 → c3 → null
+                   ↗           b           
+B:     b1 → b2 → b3
+A:          a1 → a2
+                   ↘ 
+                     c1 → c2 → c3 → null
+                   ↗                b = null, then b = a1 
+B:     b1 → b2 → b3
+       a
+            b         
+A:          a1 → a2
+                   ↘ 
+                     c1 → c2 → c3 → null
+                   ↗
+B:     b1 → b2 → b3
+            a
+                 b         
+A:          a1 → a2
+                   ↘ 
+                     c1 → c2 → c3 → null
+                   ↗ 
+B:     b1 → b2 → b3
+                 a
+A:          a1 → a2
+                   ↘ b
+                     c1 → c2 → c3 → null
+                   ↗ a
+B:     b1 → b2 → b3
+Since a == b is true, end loop while(a != b), return the intersection node a = c1.
+
+Case 3 (Have No Intersection & Same Len):
+
+       a
+A:     a1 → a2 → a3 → null
+B:     b1 → b2 → b3 → null
+       b
+            a
+A:     a1 → a2 → a3 → null
+B:     b1 → b2 → b3 → null
+            b
+                 a
+A:     a1 → a2 → a3 → null
+B:     b1 → b2 → b3 → null
+                 b
+                      a = null
+A:     a1 → a2 → a3 → null
+B:     b1 → b2 → b3 → null
+                      b = null
+Since a == b is true (both refer to null), end loop while(a != b), return a = null.
+
+Case 4 (Have No Intersection & Different Len):
+
+       a
+A:     a1 → a2 → a3 → a4 → null
+B:     b1 → b2 → b3 → null
+       b
+            a
+A:     a1 → a2 → a3 → a4 → null
+B:     b1 → b2 → b3 → null
+            b
+                 a
+A:     a1 → a2 → a3 → a4 → null
+B:     b1 → b2 → b3 → null
+                 b
+                      a
+A:     a1 → a2 → a3 → a4 → null
+B:     b1 → b2 → b3 → null
+                      b = null, then b = a1
+       b                   a = null, then a = b1
+A:     a1 → a2 → a3 → a4 → null
+B:     b1 → b2 → b3 → null
+            b                   
+A:     a1 → a2 → a3 → a4 → null
+B:     b1 → b2 → b3 → null
+       a
+                 b
+A:     a1 → a2 → a3 → a4 → null
+B:     b1 → b2 → b3 → null
+            a
+                      b
+A:     a1 → a2 → a3 → a4 → null
+B:     b1 → b2 → b3 → null
+                 a
+                           b = null
+A:     a1 → a2 → a3 → a4 → null
+B:     b1 → b2 → b3 → null
+                      a = null
+Since a == b is true (both refer to null), end loop while(a != b), return a = null.
+
+Notice that if list A and list B have the same length, this solution will terminate in no more than 1 traversal; if both lists have different lengths, this solution will terminate in no more than 2 traversals -- in the second traversal, swapping a and b synchronizes a and b before the end of the second traversal. By synchronizing a and b I mean both have the same remaining steps in the second traversal so that it's guaranteed for them to reach the first intersection node, or reach null at the same time (technically speaking, in the same iteration) -- see Case 2 (Have Intersection & Different Len) and Case 4 (Have No Intersection & Different Len).
+
+PS: There are many great explanations of this solution for various cases, I believe to visualize it can resolve most of the doubts posted previously.
+*/
