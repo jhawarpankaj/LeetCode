fb5a17ac3820cdf73caa68f956efe6b3683f4a55
Pankaj Kr Jhawar <jhawar.pankaj1990@gmail.com>
2020-07-15 13:37:48 -0700

    Update sliding-window-problems.java

--- Array/sliding-window/sliding-window-problems.java
+++ Array/sliding-window/sliding-window-problems.java
@@ -175,3 +175,33 @@ class Solution {
         return max;
     }
 }
+
+/*
+Problem 4: https://leetcode.com/problems/container-with-most-water/
+
+Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at 
+(i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+Example:
+Input: [1,8,6,2,5,4,8,3,7]
+Output: 49
+*/
+
+class Solution {
+    public int maxArea(int[] height) {
+        int max = 0;
+        for (int l = 0, h = height.length - 1; l < h; ) {
+            // Moving the smaller one forward may find a maximum area, moving the bigger one will
+            // always be bottlenecked by the smaller one.
+            if (height[l] < height[h]) {
+                max = Math.max(max, height[l] * (h - l));
+                l++;
+            } else {
+                max = Math.max(max, height[h] * (h - l));
+                h--;
+            }
+        }
+        return max;
+    }
+}
