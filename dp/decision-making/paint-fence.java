b8f325cbf24b4494f95d1faae4b1d91ea9d6ce78
Pankaj Kr Jhawar <jhawar.pankaj1990@gmail.com>
2020-09-18 16:08:10 -0500

    Update paint-fence.java

--- dp/decision-making/paint-fence.java
+++ dp/decision-making/paint-fence.java
@@ -1,9 +1,8 @@
 /*
+https://leetcode.com/problems/paint-fence/
 
 There is a fence with n posts, each post can be painted with one of the k colors.
-
 You have to paint all the posts such that no more than two adjacent fence posts have the same color.
-
 Return the total number of ways you can paint the fence.
 
 Note:
@@ -27,25 +26,49 @@ Explanation: Take c1 as color 1, c2 as color 2. All possible ways are:
 */
 
 /*   
-Interesting problem ...
-To deal with such problems, best is to come up with the generic recusrive equation first and then take care of the 
-base cases and we are done.
+If you are painting the ith post, you have two options:
 
-Assuming we want to paint fence i, two cases:
-i) it's color is different than the previous fence color.
-ii) it's color is same as the previous color. (but here we need to make sure that last 2 colors are not same.
+make it different color as i-1th post
+make it same color as i-1 th post (if you are allowed!)
+simply add these for your answer:
+num_ways(i) = num_ways_diff(i) + num_ways_same(i)
 
-i.e., f[i] = different(f[i - 1]) + same(f[i - 1])
-different(f[i - 1]) = f[i - 1] * (k - 1).
-same(f[i - 1]) = (only the cases where last 2 are different) * 1, and
+Now just think of how to calculate each of those functions.
 
-the cases where last 2 are different = f[i - 2] * (k - 1)
+The first one is easy. If you are painting the ith post, how many ways can you paint it to make it different from the i-1 th post? k-1
 
-Therefore, f[i] = f[i - 1] * (k - 1) + f[i - 2] * (k - 1) 
-                = (f[i - 1] + f[i - 2]) * (k - 1).
+num_ways_diff(i) = num_ways(i-1) * (k-1)
 
-Note: Whenever in the recurrence relation we see that the current depends only on last 2 or three, we don't really need
-to maintain a full array. Only those many variables are enough. See the second solution below.
+The second one is hard, but not so hard when you think about it.
+
+If you are painting the ith post, how many ways can you paint it to make it the same as the i-1th post? At first, we should think the answer is 
+1 -- it must be whatever color the last one was.
+
+num_ways_same(i) = num_ways(i-1) * 1
+
+But no! This will fail in the cases where painting the last post the same results in three adjacent posts of the same color! 
+We need to consider ONLY the cases where the last two colors were different. But we can do that!
+
+num_ways_diff(i-1) <- all the cases where the i-1th and i-2th are different.
+
+THESE are the cases where can just plop the same color to the end, and no longer worry about causing three in a row to be the same.
+
+num_ways_same(i) = num_ways_diff(i-1) * 1
+
+We sum these for our answer, like I said before:
+
+num_ways(i) = num_ways_diff(i) + num_ways_same(i)
+= num_ways(i-1) * (k-1) + num_ways_diff(i-1)
+
+We know how to compute num_ways_diff, so we can substitute:
+num_ways(i) = num_ways(i-1) * (k-1) + num_ways(i-2) * (k-1)
+
+We can even simplify a little more:
+
+num_ways(i) = (num_ways(i-1) + num_ways(i-2)) * (k-1)
+
+As a note, trying to intuitively understand that last line is impossible. If you think you understand it intuitively, you are fooling yourself. 
+Only the original equation makes intuitive sense.
 */
 
 class Solution {
@@ -77,6 +100,3 @@ class Solution {
         return current;
     }
 }
-   
-   
-   
