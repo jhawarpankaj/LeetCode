7e9f7286f85b30e78289998ac77a67465480502b
Pankaj Kr Jhawar <jhawar.pankaj1990@gmail.com>
2020-09-18 17:47:08 -0500

    Update paint-house.java

--- dp/decision-making/paint-house.java
+++ dp/decision-making/paint-house.java
@@ -1,13 +1,11 @@
 /*
 https://leetcode.com/problems/paint-house/
 
-There are a row of n houses, each house can be painted with one of the three colors: red, blue or green. 
-The cost of painting each house with a certain color is different. You have to paint all the houses such that 
-no two adjacent houses have the same color.
+There are a row of n houses, each house can be painted with one of the three colors: red, blue or green. The cost of painting each house with a certain color 
+is different. You have to paint all the houses such that no two adjacent houses have the same color.
 
-The cost of painting each house with a certain color is represented by a n x 3 cost matrix. For example, 
-costs[0][0] is the cost of painting house 0 with color red; costs[1][2] is the cost of painting house 1 
-with color green, and so on... Find the minimum cost to paint all houses.
+The cost of painting each house with a certain color is represented by a n x 3 cost matrix. For example, costs[0][0] is the cost of painting house 0 with color
+red; costs[1][2] is the cost of painting house 1 with color green, and so on... Find the minimum cost to paint all houses.
 
 Note:
 All costs are positive integers.
@@ -37,4 +35,58 @@ class Solution {
     int min(int a, int b, int c) {
         return Math.min(a, Math.min(b, c));
     }
+}
+
+/*
+https://leetcode.com/problems/paint-house-ii/
+
+There are a row of n houses, each house can be painted with one of the k colors. The cost of painting each house with a certain color is different. 
+You have to paint all the houses such that no two adjacent houses have the same color.
+
+The cost of painting each house with a certain color is represented by a n x k cost matrix. For example, costs[0][0] is the cost of painting house 0 with 
+color 0; costs[1][2] is the cost of painting house 1 with color 2, and so on... Find the minimum cost to paint all houses.
+
+Note:
+All costs are positive integers.
+
+Example:
+
+Input: [[1,5,3],[2,9,4]]
+Output: 5
+Explanation: Paint house 0 into color 0, paint house 1 into color 2. Minimum cost: 1 + 4 = 5; 
+             Or paint house 0 into color 2, paint house 1 into color 0. Minimum cost: 3 + 2 = 5. 
+*/
+
+class Solution {
+    public int minCostII(int[][] costs) {
+        int n = costs.length;
+        if (n == 0) return 0;
+        int[] min = getMin(costs[0]); // TODO
+        for (int i = 1; i < n; i++) {
+            int[] newMin = {Integer.MAX_VALUE, Integer.MAX_VALUE};
+            for (int j = 0; j < costs[i].length; j++) {
+                if (min[0] == costs[i - 1][j]) costs[i][j] += min[1];
+                else costs[i][j] += min[0];
+                update(newMin, costs[i][j]); // TODO
+            }
+            min = newMin;
+        }
+        return min[0];        
+    }
+    
+    int[] getMin(int[] cost) {
+        int[] ans = {Integer.MAX_VALUE, Integer.MAX_VALUE};
+        for (int x : cost) update(ans, x);
+        return ans;
+    }
+    
+    void update(int[] min, int val) {
+        if (val <= min[0]) {
+            min[1] = min[0];
+            min[0] = val;
+        } else if (val < min[1]) {
+            min[1] = val;
+        }
+    }
+    
 }
