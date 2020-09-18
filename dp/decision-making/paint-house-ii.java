cd77104449c648e2818e2a47fe76127648e275fa
Pankaj Kr Jhawar <jhawar.pankaj1990@gmail.com>
2020-09-18 17:05:55 -0500

    Update paint-house-ii.java

--- dp/decision-making/paint-house-ii.java
+++ dp/decision-making/paint-house-ii.java
@@ -4,8 +4,8 @@ https://leetcode.com/problems/paint-house-ii/
 There are a row of n houses, each house can be painted with one of the k colors. The cost of painting each house with a certain color is different. 
 You have to paint all the houses such that no two adjacent houses have the same color.
 
-The cost of painting each house with a certain color is represented by a n x k cost matrix. For example, costs[0][0] is the cost of painting house 0 with color 0; 
-costs[1][2] is the cost of painting house 1 with color 2, and so on... Find the minimum cost to paint all houses.
+The cost of painting each house with a certain color is represented by a n x k cost matrix. For example, costs[0][0] is the cost of painting house 0 
+with color 0; costs[1][2] is the cost of painting house 1 with color 2, and so on... Find the minimum cost to paint all houses.
 
 Note:
 All costs are positive integers.
@@ -18,8 +18,8 @@ Explanation: Paint house 0 into color 0, paint house 1 into color 2. Minimum cos
 */
 
 /* 
-Idea is the next house will always add to the min of the previous row, if they are not the same index else the second min.That's why we just keep track of the min, 
-second_min and index of minimum.
+Idea is the next house will always add to the min of the previous row, if they are not the same index else the second min.
+That's why we just keep track of the min, second_min and index of minimum.
 */
 
 class Solution {
