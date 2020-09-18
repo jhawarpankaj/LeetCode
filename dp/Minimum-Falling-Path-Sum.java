18102c55cd9b828554a122277fca0c6a20b65822
Pankaj Kr Jhawar <jhawar.pankaj1990@gmail.com>
2020-09-18 17:44:51 -0500

    Update Minimum-Falling-Path-Sum.java

--- dp/Minimum-Falling-Path-Sum.java
+++ dp/Minimum-Falling-Path-Sum.java
@@ -46,6 +46,61 @@ class Solution {
         int min = arr[0];
         for (int x : arr) min = Math.min(min, x);
         return min;
+    }    
+}
+
+/*
+https://leetcode.com/problems/minimum-falling-path-sum-ii/
+
+Given a square grid of integers arr, a falling path with non-zero shifts is a choice of exactly one element from each row of arr, such that no two elements 
+chosen in adjacent rows are in the same column.
+
+Return the minimum sum of a falling path with non-zero shifts.
+
+Example 1:
+Input: arr = [[1,2,3],[4,5,6],[7,8,9]]
+Output: 13
+Explanation: 
+The possible falling paths are:
+[1,5,9], [1,5,7], [1,6,7], [1,6,8],
+[2,4,8], [2,4,9], [2,6,7], [2,6,8],
+[3,4,8], [3,4,9], [3,5,7], [3,5,9]
+The falling path with the smallest sum is [1,5,7], so the answer is 13.
+ 
+Constraints:
+1 <= arr.length == arr[i].length <= 200
+-99 <= arr[i][j] <= 99
+*/
+
+class Solution {
+    public int minFallingPathSum(int[][] arr) {
+        int n = arr.length;
+        if (n == 0) return 0;
+        int[] min = getMin(arr[0]); // TODO
+        for (int i = 1; i < n; i++) {
+            int[] newMin = {Integer.MAX_VALUE, Integer.MAX_VALUE};
+            for (int j = 0; j < arr[i].length; j++) {
+                if (min[0] == arr[i - 1][j]) arr[i][j] += min[1];
+                else arr[i][j] += min[0];
+                update(newMin, arr[i][j]); // TODO
+            }
+            min = newMin;
+        }
+        return min[0];
     }
     
+    int[] getMin(int[] arr) {
+        int[] ans = {Integer.MAX_VALUE, Integer.MAX_VALUE};
+        for (int x : arr) update(ans, x);
+        return ans;
+    }
+    
+    void update(int[] arr, int val) {
+        if (val <= arr[0]) {
+            arr[1] = arr[0];
+            arr[0] = val;
+        } else if (val < arr[1]) {
+            arr[1] = val;
+        }
+    }    
 }
