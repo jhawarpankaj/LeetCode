bea42909df69c42386ff7a4942ecc7b401e42e7e
Pankaj Kr Jhawar <jhawar.pankaj1990@gmail.com>
2020-09-17 22:57:58 -0500

    Update Min-Cost-Climbing-Stairs.java

--- dp/Min-Cost-Climbing-Stairs.java
+++ dp/Min-Cost-Climbing-Stairs.java
@@ -1,9 +1,9 @@
 /*
-On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
+https://leetcode.com/problems/min-cost-climbing-stairs/
 
+On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
 Once you pay the cost, you can either climb one or two steps. You need to find minimum cost to reach the top of the floor, 
 and you can either start from the step with index 0, or the step with index 1.
-
 Example 1:
 Input: cost = [10, 15, 20]
 Output: 15
@@ -17,18 +17,17 @@ cost will have a length in the range [2, 1000].
 Every cost[i] will be an integer in the range [0, 999].
 */
 
+// Imagine you are standing at the ith stair. You can reach here either from (i - 1) or (i - 2) and if you use any of them, pay their cost.
+// dp[i] will give the minimum cost to reach stair i.
+
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
-        if(n == 0) return 0;
         int[] dp = new int[n + 1];
-        dp[0] = cost[0];
-        dp[1] = cost[1];
-        for(int i = 2; i <= n; i++){
-            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + (i == n ? 0 : cost[i]);
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.min(cost[i - 2] + dp[i - 2], cost[i - 1] + dp[i - 1]);
         }
         return dp[n];
-        
     }
 }
 
@@ -36,13 +35,15 @@ class Solution {
 // depends only on the last and second last solution.
 
 class Solution {
-  public int minCostClimbingStairs(int[] cost) {
-          int s0 = 0, s1 = 0;
-          for(int i = 0; i < cost.length; i++){
-              int m = Math.min(s0, s1) + cost[i];
-              s0 = s1;
-              s1 = m;
-          }
-          return Math.min(s0, s1);        
-      }
- }
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int a = 0, b = 0;
+        int c = 0;
+        for (int i = 2; i <= n; i++) {
+            c = Math.min(cost[i - 2] + a, cost[i - 1] + b);
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
