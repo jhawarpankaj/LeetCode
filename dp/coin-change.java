34a20bccc72e58ee53457ebd26d2cc06ac1c6e6c
Pankaj Kr Jhawar <jhawar.pankaj1990@gmail.com>
2020-09-19 12:29:00 -0500

    Update coin-change.java

--- dp/coin-change.java
+++ dp/coin-change.java
@@ -1,15 +1,15 @@
 /*
-You are given coins of different denominations and a total amount of money amount. Write a function to compute the 
-fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination 
-of the coins, return -1.
+https://leetcode.com/problems/coin-change/
+
+You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need 
+to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
 
 Example 1:
-
 Input: coins = [1, 2, 5], amount = 11
 Output: 3 
 Explanation: 11 = 5 + 5 + 1
-Example 2:
 
+Example 2:
 Input: coins = [2], amount = 3
 Output: -1
 Note:
@@ -17,32 +17,23 @@ You may assume that you have an infinite number of each kind of coin.
 */
 
 /*
-Space Optimized for Unbounded Knapsack.  
-As we trying to optimize, order of for loop doesn't matter. We choose the easier one.
+=> Unbounded Knapsack (as we have infinte supply of each coin).
+The problem looks similar to the staircase problem. We want to reach the stair (amount) and the steps we are allowed to take are given (coins).
+So, lets think for the solution in the same way: "build bottom up and solve for any stair i."
 */
+
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] V = new int[amount + 1];
+        int[] dp = new int[amount + 1];
         for (int i = 1; i <= amount; i++) {
-            int min = Integer.MAX_VALUE;
-            for (int j = 0; j < coins.length; j++) {
-                if (i - coins[j] >= 0) min = Math.min(min, V[i - coins[j]]);
+            dp[i] = Integer.MAX_VALUE;
+            for (int coin : coins) {
+                if (i - coin >= 0 && dp[i - coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+                }
             }
-            if (min == Integer.MAX_VALUE) V[i] = min;
-            else V[i] = 1 + min; // to count the no of min coins required, +1 for using the current coin.
         }
-        return V[amount] == Integer.MAX_VALUE ? -1 : V[amount];
+        if (dp[amount] == Integer.MAX_VALUE) return -1;
+        else return dp[amount];
     }
 }
-
-/* 
-
-The idea is same like stair climb. We have to reach from a source -> target (0 -> amount) with constraints that we 
-can use any combinations of coins[i]. Here, cost of using a coin is 1. We have to find the minimum number of such coins used.
- 
-1. When we have to reach from a source to target, we have to build the solution for all the amounts from 1 till target.
-2. To reach any amount, we have the choice of using only the available denominations. So have to loop for the 
-   available coins.
-   
-*/
-
