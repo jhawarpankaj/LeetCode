f89de897350e4a10a25bf5f141689190273b1e1d
Pankaj Kr Jhawar <jhawar.pankaj1990@gmail.com>
2020-09-19 17:23:36 -0500

    Update string-interleaving.java

--- dp/strings/string-interleaving.java
+++ dp/strings/string-interleaving.java
@@ -60,64 +60,51 @@ class Solution {
 }
 
 // 2D DP.
-
 class Solution {
-    int[][] dp;
     public boolean isInterleave(String s1, String s2, String s3) {
-        int n1 = s1.length();
-        int n2 = s2.length();
-        int n3 = s3.length();
+        
+        int n1 = s1.length(), n2 = s2.length(), n3 = s3.length();
         if (n1 + n2 != n3) return false;
-        dp = new int[n1 + 1][n2 + 1];
-        for (int i = 0; i <= n1; i++) {
-            // for (int j = 0; j <= n2; j++) {
-                Arrays.fill(dp[i], -1);
-            // }
+        
+        boolean[][] dp = new boolean[n1 + 1][n2 + 1];
+        
+        // when both strings are empty.
+        dp[0][0] = true;
+        
+        // when second string is empty.
+        for (int i = 1, j = 0; i <= n1; i++) {
+            if (s1.charAt(i - 1) == s3.charAt(i + j - 1) && dp[i - 1][j]) dp[i][j] = true;
         }
-        dp[0][0] = 1;
-        return helper(s1, s2, s3, n1, n2, n3);
-    }
-    
-    boolean helper(String s1, String s2, String s3, int i1, int i2, int i3) {
-        if (dp[i1][i2] != -1) return dp[i1][i2] == 1 ? true : false;
-        if (i1 == 0 && i2 == 0 && i3 == 0) return true;
-        if (i1 == 0) {
-            if (s2.charAt(i2 - 1) != s3.charAt(i3 - 1)) {
-                dp[i1][i2] = 0;
-                return false;
+        
+        // when first string is empty.
+        for (int j = 1, i = 0; j <= n2; j++) {
+            if (s2.charAt(j - 1) == s3.charAt(i + j - 1) && dp[i][j - 1]) dp[i][j] = true;
+        }
+        
+        // the general case.
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (s1.charAt(i - 1) == s3.charAt(i + j - 1)) dp[i][j] = dp[i - 1][j]; // if last char of s1 and s3 mathced.
+                if (s2.charAt(j - 1) == s3.charAt(i + j - 1) && !dp[i][j]) dp[i][j] = dp[i][j - 1]; 
+                // if last char of s1 and s3 matched but dp[i][j] is till false because dp[i - 1][j] was false, and 
+                // now last char of s2 and s3 is also same. Let's give dp[i][j] one more try and check if it gets equal.
             }
-            else return helper(s1, s2, s3, i1, i2 - 1, i3 - 1);
         }
-        else if (i2 == 0) {
-            if (s1.charAt(i1 - 1) != s3.charAt(i3 - 1)) {
-                dp[i1][i2] = 0;
-                return false;
-            }
-            else return helper(s1, s2, s3, i1 - 1, i2, i3 - 1);
-        }
-        else {
-            boolean flag1 = false;
-            boolean flag2 = false;
-            if (s1.charAt(i1 - 1) == s3.charAt(i3 - 1)) flag1 = helper(s1, s2, s3, i1 - 1, i2, i3 - 1);
-            if (s2.charAt(i2 - 1) == s3.charAt(i3 - 1)) flag2 = helper(s1, s2, s3, i1, i2 - 1, i3 - 1);
-            boolean flag = flag1 || flag2;
-            dp[i1][i2] = flag ? 1 : 0;
-            return flag;
-        }
+        
+        return dp[n1][n2];
     }
 }
 
 // We can see that we only need the last array, so space can be further optimized.
-
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        int n1 = s1.length();
-        int n2 = s2.length();
-        int n3 = s3.length();
+        int n1 = s1.length(), n2 = s2.length(), n3 = s3.length();
         if (n1 + n2 != n3) return false;
-        boolean[] dp = new boolean[n2 + 1];
+        boolean[] dp = new boolean[n2 + 1]; // can take n1 also.
         dp[0] = true;
-        for (int i = 0, j = 1; j <= n2; j++) dp[j] = s2.charAt(j - 1) == s3.charAt(j - 1) && dp[j - 1];
+        for (int i = 0, j = 1; j <= n2; j++) {
+            if (s2.charAt(j - 1) == s3.charAt(j - 1) && dp[j - 1]) dp[j] = true;
+        }
         for (int i = 1; i <= n1; i++) {
             dp[0] = s1.charAt(i - 1) == s3.charAt(i - 1) && dp[0];
             for (int j = 1; j <= n2; j++) {
