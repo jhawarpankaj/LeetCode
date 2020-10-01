7e2c5726e5670fcddeddd6502c03eecdac456a7b
Pankaj Kr Jhawar <jhawar.pankaj1990@gmail.com>
2020-10-01 12:27:28 -0500

    Update sliding-window-monoqueue.java

--- Array/sliding-window/sliding-window-monoqueue.java
+++ Array/sliding-window/sliding-window-monoqueue.java
@@ -157,6 +157,38 @@ class Solution {
 }
 
 /*
+https://leetcode.com/problems/next-greater-element-ii/
+Given a circular array (the next element of the last element is the first element of the array), print the Next Greater Number for every element. 
+The Next Greater Number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find 
+its next greater number. If it doesn't exist, output -1 for this number.
+
+Example 1:
+Input: [1,2,1]
+Output: [2,-1,2]
+Explanation: The first 1's next greater number is 2; 
+The number 2 can't find next greater number; 
+The second 1's next greater number needs to search circularly, which is also 2.
+Note: The length of given array won't exceed 10000.
+*/
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        Stack<Integer> stack = new Stack<>();        
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && nums[i] > nums[stack.peek()]) ans[stack.pop()] = nums[i];
+            stack.push(i);
+        }        
+        for (int i = 0; i < n && !stack.isEmpty(); i++) {
+            while (!stack.isEmpty() && nums[i] > nums[stack.peek()]) ans[stack.pop()] = nums[i];
+        }
+        while (!stack.isEmpty()) ans[stack.pop()] = -1;        
+        return ans;
+    }
+}
+
+/*
 Problem5: https://leetcode.com/problems/sliding-window-maximum/
 Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers 
 in the window. 
